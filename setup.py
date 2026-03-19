ad7ac80866f120f8f1bbebda9cfb527d51f235e5
GauteHolen <gaute.ah@gmail.com>
2026-03-19 22:00:38 +0100

    Pypi package

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
-from setuptools import setup, find_packages
-setup(
-   name='OML_tool',
-   version='0.1',
-   packages=find_packages(),
-   license='MIT',
-   author='Gaute Holen',
-   author_email='gaute.ah@gmail.com',
-   python_requires='>=3.9',
-   long_description=open('README.md').read(),
-   url='https://github.com/GauteHolen/OML_tool',
-   install_requires=[
-       'numpy']
-)
+from setuptools import setup
+
+
+# Metadata now lives in pyproject.toml (PEP 621).
+setup()
